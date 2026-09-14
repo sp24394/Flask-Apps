b3873af4fa399d9a4f4af5acfbb31d70747e4f4f
Seb <sp24394@my.westlake.school.nz>
2026-09-15 09:49:46 +1200

    ffffffff

--- DBConnection/db.py
+++ DBConnection/db.py
@@ -2,10 +2,18 @@ import sqlite3 as sql
 
 conn = sql.connect("DBConnection/school_library.db")
 print("Connected", conn)
+conn.row_factory = sql.Row
 cursor = conn.cursor()
 print("Cursor:", cursor)
 
-cursor.execute("select * from books")
-books = cursor.fetchall()
+cursor.execute("""INSERT INTO books 
+
+                  (title, author, genre, publication_year, pages, available_copies) 
+
+                  VALUES (?, ?, ?, ?, ?, ?)""", 
+
+               ("Aue", "Becky Manawatu", "Fiction", 2019, 344, 2)) 
+print("Added") 
+conn.commit()
 
 conn.close()
