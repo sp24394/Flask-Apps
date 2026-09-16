a87db28ee8ae20f0e5bcd38f9c4c38d770e97458
Seb <sp24394@my.westlake.school.nz>
2026-09-16 15:09:09 +1200

    f

--- DBConnection/db.py
+++ DBConnection/db.py
@@ -6,14 +6,4 @@ conn.row_factory = sql.Row
 cursor = conn.cursor()
 print("Cursor:", cursor)
 
-cursor.execute("""INSERT INTO books 
-
-                  (title, author, genre, publication_year, pages, available_copies) 
-
-                  VALUES (?, ?, ?, ?, ?, ?)""", 
-
-               ("Aue", "Becky Manawatu", "Fiction", 2019, 344, 2)) 
-print("Added") 
-conn.commit()
-
 conn.close()
